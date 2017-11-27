df8307d27c2bac61ea5f62c334ba17a95a9e2144
oci3 <oci3@oci3-To-be-filled-by-O-E-M>
2017-11-27 19:02:28 +0530

    Login and LogOut

--- greektest/src/main/java/com/greekxp/UniversityLogin.java
+++ greektest/src/main/java/com/greekxp/UniversityLogin.java
@@ -1,29 +1,49 @@
 package com.greekxp;
 
-/*import org.openqa.selenium.By;*/		
-import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;		
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;		
-public class UniversityLogin {	
-	
-	    private WebDriver driver;		
-		
-		@Test				
-		public void testEasy() {	
-			driver.get("http://demo.guru99.com/selenium/guru99home/");  
-			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
-		}	
-		@BeforeTest
-		public void beforeTest() {	
-		    driver = new FirefoxDriver();  
-		}		
-		@AfterTest
-		public void afterTest() {
-			/*driver.quit();*/
+/*import java.io.IOException;*/
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+/*import org.openqa.selenium.firefox.FirefoxDriver;*/
+
+public class UniversityLogin {
+
+	public static void main(String[] arg) throws Exception {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.browser", "/usr/bin/chrome");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		/*WebDriver driver = new FirefoxDriver();*/
+		driver.manage().window().maximize();
+		driver.get("https://qa1.greekxperience.com/");
+		System.out.println("Successfully open URL");
+		Thread.sleep(5000);
+		String expectedTitle = "GreekXperience | Homepage";
+		String actualTitle = driver.getTitle();
+		if (expectedTitle.equals(actualTitle)) {
+			System.out.println("Title matched: " +expectedTitle);
+		}else
+		{
+			System.out.println("Title not matched");
 			
-		}		
+		}
+		/*Thread.sleep(4000);*/
+				  
+		// Execute the Java Script for the element which we find out
+		((JavascriptExecutor) driver).executeScript("arguments[0].click()", driver.findElement(By.xpath("//*[@id='header-btn-login']")));
+		
+		Thread.sleep(2000);
+		driver.findElement(By.id("main-username")).clear();
+		driver.findElement(By.id("main-username")).sendKeys("admin@fiu.edu");
+		driver.findElement(By.id("main-password")).clear();
+		driver.findElement(By.id("main-password")).sendKeys("a");
+		driver.findElement(By.id("main-login-btn")).click();
+		Thread.sleep(9000);
+		driver.findElement(By.xpath("//div[@id='navbar-mobile']/ul/li/a/span")).click();
+		Thread.sleep(4000);
+	    driver.findElement(By.linkText("Logout")).click();
+	    Thread.sleep(2000);
+	    System.out.println("Logged out Successfully");
+				
+	}
 }
